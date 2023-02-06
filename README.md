a65e0d48920a052910ab0de4be9b80e6e0b8c23c
Arpad Bajzath <arpad.bajzath@gmail.com>
2023-02-06 08:35:46 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mito Vue Bootcamp
+
+Hello, and welcome!
+
+Let's get into something new. First, run `npm install` to have your dependencies installed, then run `npm run dev` to start playing around.
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+const App = defineComponent({
+  name: 'App',
+})
+export default App;
+</script>
+
+<template>
+  <div>
+    <h1 class="text-h2">Welcome to Mito Vue Bootcamp!</h1>
+    <v-card width="400">
+      <v-card-item>
+        <v-card-title>This is a title</v-card-title>
+
+        <v-card-subtitle>This is a subtitle</v-card-subtitle>
+      </v-card-item>
+
+      <v-card-text>
+        This is content
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" variant="plain">
+          Button
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
